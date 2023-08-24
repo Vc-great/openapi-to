405f4febd486ac903325fb5d32eeda79b7c442fa
vc <znro0001@126.com>
2023-08-24 17:47:33 +0800

    fix: bug

--- test/gennerateCode.spec.ts
+++ test/gennerateCode.spec.ts
@@ -6,8 +6,8 @@ const config = {
   output: path.resolve("./output"), //不使用命令行需要填写
   projects: [
     {
-      title: "数据上报及异常跟踪服务", //项目名称,用于生成目录
-      path: "http://121.37.237.10:36882/v2/api-docs", //接口文档url
+      title: "测试", //项目名称,用于生成目录
+      path: "https://petstore.swagger.io/v2/swagger.json", //接口文档url
     },
   ],
 };
